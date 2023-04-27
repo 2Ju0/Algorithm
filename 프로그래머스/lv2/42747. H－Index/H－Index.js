365b8832b0cdf42c56c3dc8bddcb8e4fbc0382d8
Juyeong Lee <lge2515@naver.com>
2023-04-28 01:01:59 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -3,9 +3,8 @@ function solution(citations) {
     citations.sort((a, b) => b - a);
     
     for (let i = 0; i < citations.length; i++) {
-        if (citations[i] >= i + 1) {
-            answer++;
-        }
+        if (citations[i] >= i + 1) answer++;
+        else break;
     }
     return answer;
 }
