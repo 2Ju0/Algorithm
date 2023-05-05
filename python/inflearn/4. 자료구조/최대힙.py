4b62652e84ab4d2512499c88c1f03d706840b4de
이주영 <lge2515@naver.com>
2023-05-05 17:16:56 +0900

    [inflearn] 최대힙

---
+++ python/inflearn/4. 자료구조/최대힙.py
@@ -0,0 +1,19 @@
+import sys
+import heapq as hq
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+
+    heap = []
+    while True:
+        n = int(input())
+
+        if n == -1:
+            break
+        elif n == 0:
+            if len(heap) != 0:
+                print(-hq.heappop(heap))
+            else:
+                print(-1)
+        else:
+            hq.heappush(heap, -n)
