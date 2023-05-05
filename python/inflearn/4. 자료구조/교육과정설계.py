eecd7c05b6bf9b78f8b06b0b7fffcdb68e271530
이주영 <lge2515@naver.com>
2023-05-05 14:21:39 +0900

    [inflearn] 교육과정설계

---
+++ python/inflearn/4. 자료구조/교육과정설계.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+
+    mandatory = input().strip()
+    n = int(input())
+
+    for i in range(n):
+        plan = input()
+        dq = deque(mandatory)
+        is_right = True
+
+        for v in plan:
+            if v in dq and dq.popleft() != v:
+                print("#%d NO" % (i + 1))
+                is_right = False
+                break
+
+        if is_right:
+            if dq:
+                print("#%d NO" % (i + 1))
+            else:
+                print("#%d YES" % (i + 1))
