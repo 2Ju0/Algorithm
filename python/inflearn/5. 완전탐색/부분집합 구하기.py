7c631597c7b1aad9a59c7498adc4a032846944c2
이주영 <lge2515@naver.com>
2023-05-05 23:07:45 +0900

    [inflearn] 부분집합 구하기

---
+++ python/inflearn/5. 완전탐색/부분집합 구하기.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def dfs(v):
+    if v == n + 1:
+        for i in range(1, n + 1):
+            if ch[i] == 1:
+                print(i, end=' ')
+        print()
+        return
+    else:
+        ch[v] = 1
+        dfs(v + 1)
+        ch[v] = 0
+        dfs(v + 1)
+
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    ch = [0] * (n + 1)
+    dfs(1)
