231827202dd5e276885d142b84f286a0a72e4ee4
이주영 <lge2515@naver.com>
2023-05-09 17:38:49 +0900

    [inflearn] 미로의 최단거리 통로

---
+++ inflearn/6. DFS, BFS/미로의 최단거리 통로.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+
+    graph = [list(map(int, input().split())) for _ in range(7)]
+    dis = [[0] * 7 for _ in range(7)]
+
+    que = deque()
+    que.append((0, 0))
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    while que:
+        x, y = que.popleft()
+
+        for i in range(4):
+            next_x = x + dx[i]
+            next_y = y + dy[i]
+
+            if 0 <= next_x < 7 and 0 <= next_y < 7 and graph[next_x][next_y] == 0:
+                graph[next_x][next_y] = 1
+                dis[next_x][next_y] = dis[x][y] + 1
+                que.append((next_x, next_y))
+
+    print(dis[6][6])
