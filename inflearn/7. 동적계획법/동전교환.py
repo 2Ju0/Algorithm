04e1ec5894ece12f7496318310bb88308b9db78b
이주영 <lge2515@naver.com>
2023-05-12 23:46:15 +0900

    [inflearn] 동전교환

---
+++ inflearn/7. 동적계획법/동전교환.py
@@ -0,0 +1,18 @@
+import sys
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+
+    n = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
+
+    dp = [1000] * (m + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, m + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    print(dp)
