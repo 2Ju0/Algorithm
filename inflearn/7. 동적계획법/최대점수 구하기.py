eca13d2a2b7f1bf628ee9fcaeecb0a01c74839ab
이주영 <lge2515@naver.com>
2023-05-21 23:59:56 +0900

    [inflearn] 최대점수 구하기

---
+++ inflearn/7. 동적계획법/최대점수 구하기.py
@@ -0,0 +1,14 @@
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    dy = [0] * (m + 1)
+
+    for i in range(n):
+        ps, pt = map(int, input().split())
+        for j in range(m, pt - 1, -1):
+            dy[j] = max(dy[j], dy[j - pt] + ps)
+
+    print(dy[m])
