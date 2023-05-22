3dc27bc3f53248b0cd1e0e0bf96584e92c08098a
이주영 <lge2515@naver.com>
2023-05-22 15:00:45 +0900

    [inflearn] 플로이드 워셜 알고리즘

---
+++ inflearn/7. 동적계획법/플로이드 워셜 알고리즘.py
@@ -0,0 +1,31 @@
+import sys
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    dp = [[5000] * (n + 1) for _ in range(n + 1)]
+
+    # 그래프 초기화
+    for i in range(1, n + 1):
+        dp[i][i] = 0
+
+    for i in range(1, m + 1):
+        s, e, w = map(int, input().split())
+        dp[s][e] = w
+
+    # (i -> j) 비용과 (i -> k -> j) 비용 비교
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i == 0 and j == 0:
+                continue
+            if dp[i][j] == 5000:
+                print('M', end=' ')
+            else:
+                print(dp[i][j], end=' ')
+        print()
